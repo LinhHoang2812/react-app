84142ea13cfd554ca72d1e0197641aa75d46e45c
LinhHoang2812 <zoeylinh2812@gmail.com>
2023-06-21 16:10:58 +0200

    first init

---
+++ src/components/Tours.js
@@ -0,0 +1,39 @@
+import Title from "./Title";
+import { tours } from "../data";
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subtitle="tours" />
+
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <article className="tour-card">
+              <div className="tour-img-container">
+                <img src={tour.url} className="tour-img" alt="" />
+                <p className="tour-date">{tour.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{tour.title}</h4>
+                </div>
+                <p>{tour.text}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>
+                    {tour.place}
+                  </p>
+                  <p>{tour.duration}</p>
+                  <p>{tour.price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Tours;
